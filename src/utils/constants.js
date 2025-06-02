8ff60848f25660ead9501a3eb2fae81d642cf556
unknown <loc842211@gmail.com>
2025-06-02 21:15:03 +0700

    fix BUILD_MODE

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,14 @@
 //export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-backend-wdel.onrender.com'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-backend-wdel.onrender.com'
+}
+export const API_ROOT = apiRoot
+//export const API_ROOT = 'https://trello-backend-wdel.onrender.com'
 
 export const DEFAULT_PAGE = 1
 export const DEFAULT_ITEMS_PER_PAGE = 12
