c73cdb25c2586eeff4514f63d64fc905c68e3102
unknown <loc842211@gmail.com>
2025-06-02 20:55:13 +0700

    thay doi node

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const API_ROOT = 'http://localhost:8017'
+//export const API_ROOT = 'http://localhost:8017'
+export const API_ROOT = 'https://trello-backend-wdel.onrender.com'
 
 export const DEFAULT_PAGE = 1
 export const DEFAULT_ITEMS_PER_PAGE = 12
