cbf5b5e76f0ab1e62b8d1d7ba471a13c579d95cf
unknown <loc842211@gmail.com>
2025-06-03 12:57:56 +0700

    fix vite confix

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
-// https://vitejs.dev/config/
+import { fileURLToPath, URL } from 'url'
+
 export default defineConfig({
   define: {
     'process.env': process.env
   },
-  plugins: [
-    react(),
-    svgr()
-  ],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [
-      { find: '~', replacement: '/src' }
+      { find: '~', replacement: '/src' },
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }
     ]
-  }
+  },
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 3000
+  },
+  // 👇 Quan trọng cho SPA routing khi deploy
+  base: '/',
 })
